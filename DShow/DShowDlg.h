1752a18b9de2600cb1866e6e688ee38b2206d08d
sheldon- <xiuying92@sina.com>
2016-07-19 16:20:00 +0800

    创建项目

---
+++ DShow/DShowDlg.h
@@ -0,0 +1,31 @@
+
+// DShowDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CDShowDlg 对话框
+class CDShowDlg : public CDialogEx
+{
+// 构造
+public:
+	CDShowDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_DSHOW_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
